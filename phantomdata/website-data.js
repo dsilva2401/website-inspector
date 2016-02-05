c6b196fb0267d21498477ab043b5e03bafae8cbe
Diego Silva <dsilva2401@gmail.com>
2016-02-05 13:09:24 -0500

    Some updates

--- phantomdata/website-data.js
+++ phantomdata/website-data.js
@@ -1,9 +1,11 @@
 var page = require('webpage').create();
-var system = require('system')
+var system = require('system');
+var fs = require('fs');
 var url = system.args[1];
 var w = system.args[2];
 var h = system.args[3];
 page.viewportSize = { width: w, height: h };
+
 
 page.open(url, function(status) {
 	console.log("Status: " + status);
@@ -13,7 +15,19 @@ page.open(url, function(status) {
 		return;
 	}
 
-	var websiteData = ''
+	// Get website data
+	var websiteData = {};
+	websiteData.url = url;
+	websiteData.title = page.evaluate(function() { return document.title; });
+	websiteData.scripts = (function () {
+		
+	})();
+
+	// Update logfile
+	// console.log(JSON.stringify);
+	// console.log(fs.write);
+	fs.write('logfile.json', JSON.stringify(websiteData))
+
 
 	phantom.exit();
 });
