2680744026cfd4a1b11b1f2eaa78784f439585be
Diego <dsilva2401@gmail.com>
2016-02-07 22:13:04 -0500

    Update eifile.sh

--- eifile.sh
+++ eifile.sh
@@ -1,9 +1,9 @@
-echo "Installing phantomjs"
+echo -e "Installing phantomjs\n"
 chmod +x install_phantomjs.sh
 sudo ./install_phantomjs.sh
 
-echo "Installing npm dependencies.."
+echo -e "Installing npm dependencies..\n"
 npm install
 
-echo "Installing bower dependencies"
+echo -e "Installing bower dependencies\n"
 bower install
